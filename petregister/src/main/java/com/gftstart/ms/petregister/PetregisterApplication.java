12b2db606bd7e434ae6be5982eedec8253ac8cd3
nivlekrod <kmrkelvin@hotmail.com>
2025-02-26 11:47:39 -0300

    configuracoes do projeto

---
+++ petregister/src/main/java/com/gftstart/ms/petregister/PetregisterApplication.java
@@ -0,0 +1,13 @@
+package com.gftstart.ms.petregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetregisterApplication.class, args);
+	}
+
+}
---
+++ petregister/src/test/java/com/gftstart/ms/petregister/PetregisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gftstart.ms.petregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetregisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
