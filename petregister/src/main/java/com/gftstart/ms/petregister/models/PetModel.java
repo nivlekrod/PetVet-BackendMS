434169d5fb7bcfd2e204f0e211ff3be173dc76b9
nivlekrod <kmrkelvin@hotmail.com>
2025-03-01 23:05:01 -0300

    mudando tipo do id

--- petregister/src/main/java/com/gftstart/ms/petregister/models/PetModel.java
+++ petregister/src/main/java/com/gftstart/ms/petregister/models/PetModel.java
@@ -6,15 +6,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+import java.util.UUID;
+
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PetModel {
+public class PetModel implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long petId;
+    private UUID petId;
 
     private String name;
 
