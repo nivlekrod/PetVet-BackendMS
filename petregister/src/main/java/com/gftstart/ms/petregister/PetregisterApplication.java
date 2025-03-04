ab9f090d75112ed68b3ca7476015cb90a2e9b997
nivlekrod <kmrkelvin@hotmail.com>
2025-03-03 21:43:32 -0300

    add e habilitando eureka client

--- petregister/src/main/java/com/gftstart/ms/petregister/PetregisterApplication.java
+++ petregister/src/main/java/com/gftstart/ms/petregister/PetregisterApplication.java
@@ -2,8 +2,10 @@ package com.gftstart.ms.petregister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PetregisterApplication {
 
 	public static void main(String[] args) {
