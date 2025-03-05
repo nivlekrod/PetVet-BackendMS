6128c98b9638c53976bfc40388546f98953a2d97
nivlekrod <kmrkelvin@hotmail.com>
2025-03-05 00:09:12 -0300

    configurando o gateway

--- cloudgateway/src/main/java/com/gftstart/ms/cloudgateway/CloudgatewayApplication.java
+++ cloudgateway/src/main/java/com/gftstart/ms/cloudgateway/CloudgatewayApplication.java
@@ -2,12 +2,25 @@ package com.gftstart.ms.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudgatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/api/pets/**").uri("lb://petregister"))
+				.route(r -> r.path("/api/appointments/**").uri("lb://appointmentscheduling"))
+				.build();
+	}
 }
