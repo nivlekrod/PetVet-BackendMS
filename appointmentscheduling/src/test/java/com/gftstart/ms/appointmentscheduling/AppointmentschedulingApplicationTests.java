cbd0b27690284b69bc6ffee72d70e9a972425b5a
nivlekrod <kmrkelvin@hotmail.com>
2025-03-05 21:53:28 -0300

    criando microservico de agendamento

---
+++ appointmentscheduling/src/test/java/com/gftstart/ms/appointmentscheduling/AppointmentschedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gftstart.ms.appointmentscheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentschedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
