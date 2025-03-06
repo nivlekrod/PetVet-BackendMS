3eeac51bcba06a961674fee03950a8483348bd98
nivlekrod <kmrkelvin@hotmail.com>
2025-03-05 21:55:22 -0300

    add PetDataDTO

---
+++ appointmentscheduling/src/main/java/com/gftstart/ms/appointmentscheduling/dtos/PetDataDTO.java
@@ -0,0 +1,29 @@
+package com.gftstart.ms.appointmentscheduling.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.gftstart.ms.appointmentscheduling.enums.Species;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PetDataDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private UUID petId;
+    private String name;
+    private Species species;
+    private String breed;
+    private Integer age;
+    private Double weight;
+    private String tutor;
+    private String emailTutor;
+}
