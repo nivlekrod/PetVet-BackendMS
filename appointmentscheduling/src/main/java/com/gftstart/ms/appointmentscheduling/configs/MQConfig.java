90d783a8a332bb7fe4b5645d5a7db0f9b2cddb03
nivlekrod <kmrkelvin@hotmail.com>
2025-03-07 20:41:06 -0300

    feat(MQConfig): adicionando Jackson2Json para converter mensagens

--- appointmentscheduling/src/main/java/com/gftstart/ms/appointmentscheduling/configs/MQConfig.java
+++ appointmentscheduling/src/main/java/com/gftstart/ms/appointmentscheduling/configs/MQConfig.java
@@ -1,6 +1,9 @@
 package com.gftstart.ms.appointmentscheduling.configs;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,5 +17,17 @@ public class MQConfig {
     @Bean
     public Queue queuePetCreated(){
         return new Queue(petCreatedQueue, true);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();
+
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); // Ignora propriedades desconhecidas
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true); // Converte strings vazias em null
+        objectMapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false); // Permite null para tipos primitivos (mas LocalTime não é um tipo primitivo)
+
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
