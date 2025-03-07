21ccb2e61ddfaca1eccc878cdd29795be7df6917
nivlekrod <kmrkelvin@hotmail.com>
2025-03-06 21:36:52 -0300

    feat: criando DTO para transferencia dos dados do evento pet_created

---
+++ appointmentscheduling/src/main/java/com/gftstart/ms/appointmentscheduling/dtos/PetCreatedEventDTO.java
@@ -0,0 +1,35 @@
+package com.gftstart.ms.appointmentscheduling.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.gftstart.ms.appointmentscheduling.enums.Species;
+import com.gftstart.ms.appointmentscheduling.models.PetModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PetCreatedEventDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private UUID petId;
+    private String name;
+    private Species species;
+    private String breed;
+    private Integer age;
+    private Double weight;
+    private String tutor;
+    private String emailTutor;
+
+    public PetCreatedEventDTO(PetModel pet) {
+        BeanUtils.copyProperties(pet, this);
+    }
+}
