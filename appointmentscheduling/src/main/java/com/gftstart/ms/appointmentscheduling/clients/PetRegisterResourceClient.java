70cf6230226b0adcd937b295902e4849403598b3
nivlekrod <kmrkelvin@hotmail.com>
2025-03-07 02:53:14 -0300

    feat: adicionando e configurando o openfeign

---
+++ appointmentscheduling/src/main/java/com/gftstart/ms/appointmentscheduling/clients/PetRegisterResourceClient.java
@@ -0,0 +1,16 @@
+package com.gftstart.ms.appointmentscheduling.clients;
+
+import com.gftstart.ms.appointmentscheduling.models.PetModel;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@FeignClient(value = "petregister", path = "/api/pets")
+public interface PetRegisterResourceClient {
+
+    @GetMapping("/{id}")
+    ResponseEntity<PetModel> getPetById(@PathVariable("id") UUID id);
+}
