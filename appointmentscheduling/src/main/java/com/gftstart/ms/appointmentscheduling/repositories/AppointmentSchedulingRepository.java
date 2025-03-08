2f9f546acdf3c37eba13ce8acdec94fce81bcd14
nivlekrod <kmrkelvin@hotmail.com>
2025-03-07 21:08:09 -0300

    feat: adicionando metodo de busca

--- appointmentscheduling/src/main/java/com/gftstart/ms/appointmentscheduling/repositories/AppointmentSchedulingRepository.java
+++ appointmentscheduling/src/main/java/com/gftstart/ms/appointmentscheduling/repositories/AppointmentSchedulingRepository.java
@@ -2,8 +2,6 @@ package com.gftstart.ms.appointmentscheduling.repositories;
 
 import com.gftstart.ms.appointmentscheduling.models.AppointmentSchedulingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,9 +9,9 @@ import java.util.UUID;
 
 @Repository
 public interface AppointmentSchedulingRepository extends JpaRepository<AppointmentSchedulingModel, UUID> {
-
     List<AppointmentSchedulingModel> findByPetModel_PetId(UUID petId);
 
-    boolean existsByPetModel_PetId(UUID petId);
+    List<AppointmentSchedulingModel> findAllByPetModel_PetId(UUID petId);
 
+    boolean existsByPetModel_PetId(UUID petId);
 }
